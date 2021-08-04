53617a9379727855cc917162591e2f054f8dec22
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-08-04 22:25:50 +0200

    added a http example for images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let marvel = require('marvel-characters');
+let https = require ('https');
+
 console.log(marvel()); //test to see if it works
 let fs = require ('fs');
 let PDFDocument = require('pdfkit');
@@ -15,3 +17,9 @@ makePDF.font('Times-Roman')
     .fontSize(25)
     .text(spdfText, 50, 50);
 makePDF.end();
+
+
+let photoURL = 'https://miro.medium.com/max/1200/1*m5RYM_Wkj4LsZewpigV5tg.jpeg';
+https.get(photoURL, (res) =>{
+    res.pipe(fs.createWriteStream(__dirname + '/nodejs-logo.jpeg'))
+})
